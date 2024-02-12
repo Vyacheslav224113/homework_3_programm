73d5eb47d5827fcd5434aadd5dac06e33adb9473
Slava Korolev <vyacheslav.korolev.ru@mail.ru>
2024-02-12 21:09:50 +0700

    Task_2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+
+
+
+
+
+// Задача 2: Задайте массив заполненный случайными
+// трёхзначными числами. Напишите программу,
+// которая покажет количество чётных чисел в
+// массиве.
+
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+int GetCount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32 (Console.ReadLine());
+int[] res = CreateArray(N);
+Console.WriteLine($"Массив: [{string.Join(";", res)}]");
+Console.WriteLine($"Результат: {GetCount(res)}");
