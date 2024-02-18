6bdb7fd14274b855172c1188a394be92b3d30e8c
Slava Korolev <vyacheslav.korolev.ru@mail.ru>
2024-02-18 17:11:01 +0700

    Task_1

--- Program.cs
+++ Program.cs
@@ -1,7 +1,34 @@
 ﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
 // Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
 
-
+while (true)
+{
+    Console.Write("Введние текст: ");
+    string text = Console.ReadLine();
+    if(text == "q")
+    {
+        break;
+    }
+    int number;
+    if(int.TryParse(text, out number))
+    {
+        int sum = 0;
+        while(number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        if(sum % 2 == 0)
+        {
+            Console.WriteLine("Stop");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод, введите q или целое число");
+        }
+    }
+}
 
 
 
@@ -12,30 +39,31 @@
 // массиве.
 
 
-int[] CreateArray(int size)
-{
-    int[] array = new int[size];
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(100, 1000);
-    }
-    return array;
-}
-int GetCount(int[] arr)
-{
-    int count = 0;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if(arr[i] % 2 == 0)
-        {
-            count++;
-        }
-    }
-    return count;
-}
+// int[] CreateArray(int size)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(100, 1000);
+//     }
+//     return array;
+// }
+// int GetCount(int[] arr)
+// {
+//     int count = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if(arr[i] % 2 == 0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
 
-Console.Write("Введите число: ");
-int N = Convert.ToInt32 (Console.ReadLine());
-int[] res = CreateArray(N);
-Console.WriteLine($"Массив: [{string.Join(";", res)}]");
-Console.WriteLine($"Результат: {GetCount(res)}");
+// Console.Write("Введите число: ");
+// int N = Convert.ToInt32 (Console.ReadLine());
+// int[] res = CreateArray(N);
+// Console.WriteLine($"Массив: [{string.Join(";", res)}]");
+// Console.WriteLine($"Результат: {GetCount(res)}");
+
