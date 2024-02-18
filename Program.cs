1e1cb6d5ed6a978db72f1ad6b7ea984802ad09ab
Slava Korolev <vyacheslav.korolev.ru@mail.ru>
2024-02-18 17:22:22 +0700

    Task_3

--- Program.cs
+++ Program.cs
@@ -1,34 +1,34 @@
 ﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
 // Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
 
-while (true)
-{
-    Console.Write("Введние текст: ");
-    string text = Console.ReadLine();
-    if(text == "q")
-    {
-        break;
-    }
-    int number;
-    if(int.TryParse(text, out number))
-    {
-        int sum = 0;
-        while(number > 0)
-        {
-            sum += number % 10;
-            number /= 10;
-        }
-        if(sum % 2 == 0)
-        {
-            Console.WriteLine("Stop");
-            break;
-        }
-        else
-        {
-            Console.WriteLine("Некорректный ввод, введите q или целое число");
-        }
-    }
-}
+// while (true)
+// {
+//     Console.Write("Введние текст: ");
+//     string text = Console.ReadLine();
+//     if(text == "q")
+//     {
+//         break;
+//     }
+//     int number;
+//     if(int.TryParse(text, out number))
+//     {
+//         int sum = 0;
+//         while(number > 0)
+//         {
+//             sum += number % 10;
+//             number /= 10;
+//         }
+//         if(sum % 2 == 0)
+//         {
+//             Console.WriteLine("Stop");
+//             break;
+//         }
+//         else
+//         {
+//             Console.WriteLine("Некорректный ввод, введите q или целое число");
+//         }
+//     }
+// }
 
 
 
@@ -67,3 +67,31 @@ while (true)
 // Console.WriteLine($"Массив: [{string.Join(";", res)}]");
 // Console.WriteLine($"Результат: {GetCount(res)}");
 
+
+
+
+// Задача 3: Напишите программу, которая перевернёт
+// одномерный массив (первый элемент станет
+// последним, второй – предпоследним и т.д.)
+
+static void Main()
+ int[] numbers = { 1, 3, 5, 6, 7, 8 };
+ int temp;
+{
+    Console.Write("Исходный массив: ");
+    foreach (int number in numbers)
+    {
+        Console.Write(number + " ");
+    }
+    for (int i = 0; i < numbers.Length / 2; i++)
+    {
+        temp = numbers[i];
+        numbers[i] = numbers[numbers.Length - 1 - i];
+        numbers[numbers.Length - 1 - i] = temp;
+    }
+    Console.Write("Перевернутый массив: ");
+    foreach (int number in numbers)
+    {
+        Console.Write(number + " ");
+    }
+}
